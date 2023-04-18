02ff55dc428b86ff2d715641d313b1c427fadf9b
Vlad <vlad@MBP-Vlad.cn.ru_novotelecom.ru>
2023-04-18 21:03:42 +0700

    all quests

---
+++ app/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+import datetime
+from os import listdir
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir'),
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now().time()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    files = listdir('.')
+    return HttpResponse(f'Содержимое рабочей директории: {files}')
